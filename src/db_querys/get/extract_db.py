c4260009d787aaa19f749cde0017b2b48ad6069f
Vadim <espvsvs1@gmail.com>
2024-07-19 15:21:28 +0500

    Добавил в extract_db() возможность сортировки

--- src/db_querys/get/extract_db.py
+++ src/db_querys/get/extract_db.py
@@ -1,17 +1,25 @@
 import sqlite3
 
 
-def extract_db(select_column: str, path_db: str, table_name: str) -> list[tuple]:
+def extract_db(select_column: str, path_db: str, table_name: str, order_by: str = None) -> list[tuple]:
     """ Извлекает из БД список с содержимым.
     path_db (str) - путь к базе данных;
-    table_name (str) - имя листа БД;
+    table_name (str) - имя таблицы БД;
+    order_by (str) - имя столбца для сортировки (опционально).
     """
 
     # Подключаемся к базе данных SQLite
     with sqlite3.connect(path_db) as conn:
         # Создаем объект курсора
         cursor = conn.cursor()
-        # Используем курсор для выполнения запросов
-        cursor.execute(f'SELECT {select_column} FROM {table_name}')
+
+        # Формируем SQL-запрос с учетом возможной сортировки
+        if order_by:
+            query = f'SELECT {select_column} FROM {table_name} ORDER BY {order_by}'
+        else:
+            query = f'SELECT {select_column} FROM {table_name}'
+
+        # Используем курсор для выполнения запроса
+        cursor.execute(query)
         result = cursor.fetchall()
         return result
--- src/organizer/interface/wb_timer.py
+++ src/organizer/interface/wb_timer.py
@@ -17,6 +17,11 @@ from win10toast import ToastNotifier
 from datetime import datetime, timedelta
 from src.organizer.checking_schedule.day.base_check import base_check
 from src.organizer.checking_schedule.day.dur_shift_check import dur_shift_check
+from src.shared.xlsx_utils.parse_table import parse_table
+from src.db_querys.get.extract_db import extract_db
+from src.db_querys.get.get_days_from_db import get_days_from_db
+
+
 
 class CountdownTimer(tk.Tk):
     """
@@ -175,8 +180,6 @@ class CountdownTimer(tk.Tk):
         path_d_now: str = path_to_now + '\\' + name_file
 
         # Проверяем, открыт ли уже файл today
-        # TODO: !!! Функция не сработает, если wps ранее был уже открыт,
-        #  чтобы исправить - нужно закрыть сначала wps, затем уже открыть
         check_result: list[bool, str] = [False, '']
         if is_excel_file_open(path_d_now):
             # Выполняем базовую проверку новой версии файла
@@ -187,9 +190,21 @@ class CountdownTimer(tk.Tk):
                                       sleep_time='21:30', min_wb=10,
                                       planning_dur='00:10',
                                       path_db=path_to_db, wb_table_name='wb')
-            
+            # Получаем текущую дату
+            today_date = datetime.now()
+            # Форматируем дату в нужный формат 'dd.mm.yy'
+            today_date = today_date.strftime('%d.%m.%y')
+
+            get_days_from_db(today_date, 'last')
+
+            # Извлекаем old_shedule, new_shedule, all_wb в виде списков кортежей,
+            #   в целях дальнейшей проверки нового расписания
+            old_shedule: list[tuple] =
+            new_shedule: list[tuple] = parse_table(path_d_now)
+            all_wb: list[tuple] = extract_db(select_column='*', path_db=path_to_db, table_name='wb')
+
             # TODO: Выполняем продвинутую проверку для сегодняшнего расписания
-            # check_result_two = dur_shift_check(old_shedule=, new_shedule=, all_wb=)
+            check_result_two = dur_shift_check(old_shedule=, new_shedule=, all_wb=)
 
             # Получаем результат с учетом обоих проверок
             check_result[0] = check_result_one[0] and check_result_two[0]
